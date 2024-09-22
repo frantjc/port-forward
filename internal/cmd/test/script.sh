8accfa6a5e9e27bb9505e30625ac6978a9785dd0
Frantjc <logsdonalex11@gmail.com>
2024-09-22 19:07:30 -0400

    lost

---
+++ internal/cmd/test/script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+iptables -t nat -A POSTROUTING --source 192.168.0.3 --destination 192.168.0.1 --jump SNAT --to-source 192.168.0.203
+
+upnpc -a 192.168.0.203 80 80 TCP
+
+iptables -t nat -D POSTROUTING --source 192.168.0.3 --destination 192.168.0.1 --jump SNAT --to-source 192.168.0.203
