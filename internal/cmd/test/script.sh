1b1ec36eabaa7e6811c525ccd0bff4edb27ebe0f
Frantjc <logsdonalex11@gmail.com>
2024-09-22 19:12:41 -0400

    202

--- internal/cmd/test/script.sh
+++ internal/cmd/test/script.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-iptables -t nat -A POSTROUTING --source 192.168.0.3 --destination 192.168.0.1 --jump SNAT --to-source 192.168.0.203
+iptables -t nat -A POSTROUTING --source 192.168.0.3 --destination 192.168.0.1 --jump SNAT --to-source 192.168.0.202
 
 sleep 10
 
-upnpc -a 192.168.0.203 80 80 TCP
+upnpc -a 192.168.0.202 80 80 TCP
 
 upnpc -d 80 TCP
 
-iptables -t nat -D POSTROUTING --source 192.168.0.3 --destination 192.168.0.1 --jump SNAT --to-source 192.168.0.203
+iptables -t nat -D POSTROUTING --source 192.168.0.3 --destination 192.168.0.1 --jump SNAT --to-source 192.168.0.202
